3306e2c84c28ff4f7d5cdb4864818a3f3a556c7a
deityhp <deityhp@gmail.com>
2018-11-30 14:44:22 +0800

    2018年11月29日

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-# -*- coding: utf-8 -*-
-#from fangtianxia import spider_1
-from anjuke import spider_2
+#coding=utf8
+# import  itchat
+# itchat.auto_login(hotReload=True)   #热启动你的微信
+# #itchat.run()
+# rooms=itchat.get_chatrooms(update=True)
+# for i in range(len(rooms)):
+#     print(rooms[i])   #查看你多有的群
+#
+# room = itchat.search_friends(name=r'爱宝宝')  #这里输入你好友的名字或备注。
+# print(room)
+# userName = room[0]['UserName']
+# f="./23.py"  #图片地址
+# try:
+#     itchat.send_file(f,toUserName=userName)  #如果是其他文件可以直接send_file
+#     print("success")
+# except:
+#     print("fail")
 
-#spider_1('http://esf.km.fang.com/')
-#spider_1('http://esf.km.fang.com/house/c280-d2200-j290-k2150-t21-h316-l3110/')
-#spider_2('https://km.anjuke.com/sale/d119-o5-p1/?from_area=90&to_area=150&from_price=80&to_price=200')
-spider_2('https://km.anjuke.com/sale/d119-o5-p1/')
+
+from bs4 import BeautifulSoup
+import requests
+url ='http://mp.weixin.qq.com/mp/jsreport?1=1&key=2&content=biz:MzA5NTQ0MTEyNw==,mid:2652180661,uin:777[key2]ajax_err&r=0.18012391988602539'
