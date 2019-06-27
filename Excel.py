4e85f78c9950b1b97d9cfde3fa8ccbd0c88f055d
deityhp <deityhp@gmail.com>
2019-06-27 08:32:29 +0800

    2019年06月27日-stock

---
+++ Excel.py
@@ -0,0 +1,17 @@
+import openpyxl
+wb = openpyxl.load_workbook("2018.xlsx")
+sheet = wb.worksheets[0]
+print(sheet.cell(1,1))
+
+for x in range(1,20):
+    for y in range(1,8):
+        sheet.cell(x,y).value = ""
+
+
+
+for i in range(1,20):
+    rows = sheet[i]
+
+    for row in rows:
+        print(row.value)
+        print(row)
